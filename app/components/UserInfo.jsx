0403fb8a4bdfbe8f13a50b52f3b0206282b26614
Srilekha-03 <srilekhakasha47@gmail.com>
2024-11-15 09:01:20 +0530

    userinfo styling done finally

--- app/components/UserInfo.jsx
+++ app/components/UserInfo.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import Image from 'next/image';
 
 const UserInfo = ({ userInfo }) => {
-  // Check if userInfo is undefined or null
+  console.log(userInfo)
   if (!userInfo) {
     return <div>Loading user info...</div>;  // Display loading message when userInfo is not yet available
   }
 
   // Check if userName and userimage exist
-  const { userName = 'Unknown User', userimage = 'default-image-url' } = userInfo;
+  const { userName = 'Unknown User', userimage = 'default-image-url', email ="unknown email" } = userInfo;
 
   return (
-    <div>
-      <h2>{userName}</h2>  {/* Display userName, fallback to 'Unknown User' */}
-      <img src={userimage} alt={userName} />  {/* Display userImage, fallback to default image */}
+    <div className='flex flex-col items-center gap-4'>
+        <Image src={userimage} alt='userImage' width={120} height={120} className='rounded-full mt-4'/>
+        <h2 className='text-[30px]
+        font-semibold'>{userName}</h2>
+        <h2 className='text-gray-500'>{email}</h2>
+        <button className='bg-gray-200
+         p-2 px-3 font-semibold mt-4 rounded-full'>Share</button> 
     </div>
   );
 };
